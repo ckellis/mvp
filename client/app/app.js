27a621888e264749db8e57a98dd08872bd7c651c
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-11-09 16:13:39 -0800

    Attempts at making routing work properly

--- client/app/app.js
+++ client/app/app.js
@@ -1,16 +1,70 @@
-var app = angular.module('app', ["ui.router"]);
-app.config(function($stateProvider, $urlRouterProvider){
+var app = angular.module('app', [
+  'ui.router'
+])
+.config(function ($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise("/home");
 
   $stateProvider
     .state('home', {
-        url: "/home",
-        templateUrl: "./home.html"
+        url: '/home',
+        templateUrl: './home.html',
+        controller: 'postController'
     })
 
     .state('post', {
-        url: "/post",
-        templateUrl: "./posts.html"
+        url: '/post',
+        templateUrl: './posts.html'
     });
+});
+
+app.factory('Posts', function ($http) {
+
+  var getAll = function () {
+    return $http({
+      method: 'GET',
+      url: '/home'
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+  };
+
+  var addPost = function (post) {
+    return $http({
+      method: 'POST',
+      url: '/post',
+      data: post
+    });
+  };
+
+  return {
+    getAll: getAll,
+    addPost: addPost
+  };
+});
+
+app.controller('postController', function ($scope, $location, Posts) {
+  $scope.data = {};
+  $scope.getPosts = function () {
+    Posts.getAll()
+      .then(function (posts) {
+        $scope.data.posts = posts;
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  };
+  $scope.getPosts();
+
+  $scope.post = {};
+  $scope.newPost = function () {
+    Posts.addPost($scope.post)
+      .then(function (err) {
+        $location.path('/home');
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  };
 });
